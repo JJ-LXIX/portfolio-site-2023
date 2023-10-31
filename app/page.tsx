1220091be6000b3ec7de33ca05eda0da6c476ed1
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-10-31 22:28:39 +0530

    added react lenis to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,26 @@
+"use client";
 import FooterSection from "@/components/FooterSection/FooterSection";
+import HeroText from "@/components/HeroText/HeroText";
+import SpaceBoiModel from "@/components/R3FModel/SpaceBoiModel";
 import WorkSection from "@/components/WorkSection/WorkSection";
+import { useScreenSize } from "@/utils/hooks/useScreenSize";
+import { ReactLenis } from "@studio-freight/react-lenis";
 
 export default function Home() {
+  const laptopPlus = useScreenSize();
+
   return (
-    <main className="min-h-screen">
+    <ReactLenis root>
+      <main className="min-h-screen">
+        <div className="h-screen w-full relative">
           <HeroText />
 
           {/* Space Model */}
           {laptopPlus ? <SpaceBoiModel /> : null}
-      <WorkSection />
-      <FooterSection />
-    </main>
+        </div>
+        <WorkSection />
+        <FooterSection />
+      </main>
+    </ReactLenis>
   );
 }
