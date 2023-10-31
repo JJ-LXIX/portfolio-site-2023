b634a59106a21b36eab35629886da8bf733fd0f8
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-10-31 09:25:06 +0530

    Created FooterSection Component

---
+++ components/FooterSection/FooterSection.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+export default function FooterSection({}: Props) {
+  return (
+    <div className="h-[30vh] bg-black w-full text-white">FooterSection</div>
+  );
+}
