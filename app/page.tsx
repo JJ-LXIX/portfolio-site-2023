25810ee5bb44d0f844ff767589365b403b11bcff
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-11-02 00:10:04 +0530

    added CustomCursor and SmileySVG to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 "use client";
+import CustomCursor from "@/components/CustomCursor/CustomCursor";
 import FooterSection from "@/components/FooterSection/FooterSection";
 import HeroText from "@/components/HeroText/HeroText";
 import SpaceBoiModel from "@/components/R3FModel/SpaceBoiModel";
 import WorkSection from "@/components/WorkSection/WorkSection";
 import { useScreenSize } from "@/utils/hooks/useScreenSize";
 import { ReactLenis } from "@studio-freight/react-lenis";
+import { SmileySVG } from "@/components/SmileySVG/SmileySVG";
 
 export default function Home() {
   const laptopPlus = useScreenSize();
-
   return (
     <ReactLenis root>
       <main className="min-h-screen">
@@ -20,6 +21,16 @@ export default function Home() {
         </div>
         <WorkSection />
         <FooterSection />
+
+        {/* Custom Cursor */}
+        {laptopPlus ? (
+          <>
+            <CustomCursor />
+            <SmileySVG />
+          </>
+        ) : null}
+
+        {/* Smiley SVG */}
       </main>
     </ReactLenis>
   );
