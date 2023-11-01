9570762824a8ac8c034079461877621ff70e60b3
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-11-02 00:05:58 +0530

    added throttle to handle resize

--- utils/hooks/useScreenSize.tsx
+++ utils/hooks/useScreenSize.tsx
@@ -1,14 +1,25 @@
 import { useEffect, useState } from "react";
+import throttle from "lodash.throttle";
 
 export const useScreenSize = () => {
   const [laptopPlus, setLaptopPlus] = useState(false);
 
-  useEffect(() => {
+  function handleResize() {
     if (window.innerWidth >= 1024) {
       setLaptopPlus(true);
     } else {
       setLaptopPlus(false);
     }
+  }
+
+  const throttledHandleResize = throttle(handleResize, 500);
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", throttledHandleResize);
+
+    return () => {
+      window.removeEventListener("resize", throttledHandleResize);
+    };
   }, []);
 
   return laptopPlus;
