35455efa67c33cd2eba0700f0e171b821729eaef
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-11-02 00:06:45 +0530

    created work card component

---
+++ components/WorkSection/WorkCard.tsx
@@ -0,0 +1,86 @@
+import React, { Dispatch, SetStateAction, useState } from "react";
+import Image, { StaticImageData } from "next/image";
+import { motion, AnimatePresence } from "framer-motion";
+import Link from "next/link";
+
+type Props = {
+  work: {
+    title: string;
+    description: string;
+    image1: StaticImageData;
+    techStack: string[];
+    classes: string;
+    link: string;
+    githubLink: string;
+  };
+  index: number;
+  setClickable: Dispatch<SetStateAction<boolean>>;
+};
+
+export default function WorkCard({ work, index, setClickable }: Props) {
+  const [hovered, setHovered] = useState(false);
+
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 200 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.3, delay: index % 2 === 0 ? 0 : 0.3 }}
+      viewport={{ once: true }}
+      key={work.title}
+      className={work.classes}
+      onMouseOver={() => setHovered(true)}
+      onMouseOut={() => setHovered(false)}
+    >
+      <AnimatePresence>
+        {hovered ? (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.3 }}
+            exit={{ opacity: 0 }}
+            className="absolute inset-0 bg-black/70 z-20"
+          >
+            {/* Title */}
+            <div className="border-2 py-2 px-4 rounded-2xl text-white absolute left-5 top-5 border-white text-lg">
+              {work.title}
+            </div>
+            {/* Arrow */}
+            <Link href={work.link} target="_blank" rel="noopener noreferrer">
+              <div className="icon">
+                <div className="arrow"></div>
+              </div>
+            </Link>
+            {/*Tech Stack */}
+            <div className="absolute bottom-5 left-2 3xl:left-5 flex space-x-2">
+              {work.techStack.map((tech) => (
+                <div
+                  key={tech}
+                  className="border-2 py-2 px-2 3xl:px-4 border-white rounded-2xl text-white flex items-center justify-center"
+                >
+                  {tech}
+                </div>
+              ))}
+            </div>
+            {/* Github Link */}
+            <Link
+              href={work.githubLink}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="absolute  py-2 px-4 rounded-2xl cursor-pointer text-lg font-semibold bg-blue-300 animate-pulse text-black right-5 top-5 hover:scale-110 active:scale-95 transition duration-300">
+                Github
+              </div>
+            </Link>
+          </motion.div>
+        ) : null}
+      </AnimatePresence>
+
+      <Image
+        className="object-cover"
+        fill
+        src={work.image1}
+        alt={work.description}
+      />
+    </motion.div>
+  );
+}
