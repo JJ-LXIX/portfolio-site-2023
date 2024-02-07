7c88263e1fa30c2da55ff10ea3d268f037776785
Jagrath Shetty <jagrathshettylxix@gmail.com>
2024-02-07 16:33:31 +0530

    removed suspense fallback

--- components/R3FModel/SpaceBoiModel.tsx
+++ components/R3FModel/SpaceBoiModel.tsx
@@ -1,7 +1,5 @@
 import { SpaceModel } from "@/components/Space_boi";
 import { Canvas } from "@react-three/fiber";
-import { Suspense } from "react";
-import LoadingScreen from "../LoadingScreen/LoadingScreen";
 
 type Props = {};
 
@@ -9,10 +7,8 @@ export default function SpaceBoiModel({}: Props) {
   return (
     <>
       <Canvas>
-        <Suspense fallback={<LoadingScreen />}>
-          <ambientLight />
-          <SpaceModel />
-        </Suspense>
+        <ambientLight />
+        <SpaceModel />
       </Canvas>
     </>
   );
