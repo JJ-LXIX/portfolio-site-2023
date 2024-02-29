5e0d0fde1f7ec5b4b7a1108dedf33a5861524066
Jagrath Shetty <jagrathshettylxix@gmail.com>
2024-02-29 19:03:50 +0530

    added animations to text and buttons

--- components/HeroText/HeroText.tsx
+++ components/HeroText/HeroText.tsx
@@ -1,10 +1,5 @@
 import { useRef } from "react";
-import {
-  useScroll,
-  useMotionValueEvent,
-  motion,
-  useTransform,
-} from "framer-motion";
+import { useScroll, motion, useTransform } from "framer-motion";
 import Link from "next/link";
 
 type Props = {};
@@ -18,9 +13,12 @@ export default function HeroText({}: Props) {
   const y = useTransform(scrollY, [0, 1100], ["0%", "80%"]);
   const y2 = useTransform(scrollY, [0, 1100], ["0", "-200%"]);
   return (
-    <div className="absolute inset-0  z-20" ref={ref}>
+    <div className="absolute inset-0 z-20 overflow-x-hidden" ref={ref}>
       <motion.h1
         style={{ y }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
         className="text-center md:text-left text-[5rem] md:text-[9rem] lg:text-[10rem] 2xl:text-[12rem] 3xl:text-[13rem] md:ml-5  text-white flex flex-col"
       >
         Frontend
@@ -28,13 +26,21 @@ export default function HeroText({}: Props) {
       </motion.h1>
       <motion.h2
         style={{ y: y2 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
         className=" w-full md:w-auto text-center md:text-right text-[5rem] md:text-[9rem] lg:text-[10rem] 3xl:text-[10rem] text-white absolute flex flex-col md:right-10 bottom-7 lg:bottom-10"
       >
         Jagrath <span className="-mt-8 md:-mt-14 lg:-mt-16">Shetty</span>
       </motion.h2>
 
       {/* CTA Button */}
-      <div className="absolute px-10 lg:left-10 w-full md:w-auto bottom-64 md:bottom-[26rem] lg:bottom-56 3xl:left-10 3xl:bottom-40 flex justify-around space-x-5 lg:space-x-5 3xl:space-x-5 border-white text-white text-lg md:text-3xl lg:text-2xl 3xl:text-3xl">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 0.8 }}
+        className="absolute px-10 lg:left-10 w-full md:w-auto bottom-64 md:bottom-[26rem] lg:bottom-56 3xl:left-10 3xl:bottom-40 flex justify-around space-x-5 lg:space-x-5 3xl:space-x-5 border-white text-white text-lg md:text-3xl lg:text-2xl 3xl:text-3xl"
+      >
         <Link
           href="https://github.com/JJ-LXIX"
           target="_blank"
@@ -53,7 +59,7 @@ export default function HeroText({}: Props) {
         >
           Contact
         </button>
-      </div>
+      </motion.div>
     </div>
   );
 }
