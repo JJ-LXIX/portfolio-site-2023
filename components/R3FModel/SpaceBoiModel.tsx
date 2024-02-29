cbc7e722873370c0ea216b3cb01b8b7058437b35
Jagrath Shetty <jagrathshettylxix@gmail.com>
2024-02-29 19:04:10 +0530

    added entry animation to spacemodel

--- components/R3FModel/SpaceBoiModel.tsx
+++ components/R3FModel/SpaceBoiModel.tsx
@@ -1,15 +1,21 @@
-import { SpaceModel } from "@/components/Space_boi";
+import { SpaceModel } from "@/components/R3FModel/Space_boi";
 import { Canvas } from "@react-three/fiber";
+import { motion } from "framer-motion-3d";
 
 type Props = {};
 
 export default function SpaceBoiModel({}: Props) {
   return (
-    <>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 2 }}
+      className="h-screen w-full relative"
+    >
       <Canvas>
         <ambientLight />
         <SpaceModel />
       </Canvas>
-    </>
+    </motion.div>
   );
 }
